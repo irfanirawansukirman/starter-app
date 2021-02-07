de2755f7b906248b89074a26c6d8f51acf754496
Irfan Irawan Sukirman <dadang.kotz@gmail.com>
2021-02-07 16:49:23 +0700

    2.1

---
+++ app/src/test/java/com/irfanirawansukirman/starterapp/Doc2.kt
@@ -0,0 +1,7 @@
+package com.irfanirawansukirman.starterapp
+
+/**
+ * Created by Irfan Irawan Sukirman on 1/26/21 at Bandung City
+ * Copyright (c) 2021 PT. Haruka Evolusi Digital Utama. All rights reserved.
+ */
+class Doc2(var value: Int = 0)
--- app/src/test/java/com/irfanirawansukirman/starterapp/ExampleUnitTest.kt
+++ app/src/test/java/com/irfanirawansukirman/starterapp/ExampleUnitTest.kt
@@ -1,5 +1,7 @@
 package com.irfanirawansukirman.starterapp
 
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -13,5 +15,28 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun `test simple with mockk`() {
+        val doc1 = mockk<Doc1>()
+        val doc2 = mockk<Doc2>()
+
+        every { doc1.value } returns 6
+        every { doc2.value } returns 6
+
+        val suc = Suc(doc1, doc2)
+
+        assertEquals(12, suc.calculate())
+    }
+
+    @Test
+    fun `test simple with mockk2`() {
+        val doc1 = Doc1(6)
+        val doc2 = Doc2(6)
+
+        val suc = Suc(doc1, doc2)
+
+        assertEquals(12, suc.calculate())
     }
 }
