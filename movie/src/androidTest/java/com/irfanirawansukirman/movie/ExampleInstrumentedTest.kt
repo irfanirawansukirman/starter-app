708f29b49a080d6973f3b8ee42ac05a5bc3d6e72
Irfan Irawan Sukirman <dadang.kotz@gmail.com>
2021-03-09 16:42:44 +0700

    3.1

---
+++ movie/src/androidTest/java/com/irfanirawansukirman/movie/ExampleInstrumentedTest.kt
@@ -0,0 +1,25 @@
+package com.irfanirawansukirman.movie
+
+import android.content.Intent
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import com.irfanirawansukirman.movie.presentation.MovieActivity
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+
+class ExampleInstrumentedTest {
+}
